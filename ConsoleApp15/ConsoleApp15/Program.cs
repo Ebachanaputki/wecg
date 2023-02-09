bddb7d5608f94e3beedf46d5672d39b7dcacd83e
KolyoLudiq <105170253+Ebachanaputki@users.noreply.github.com>
2023-02-09 10:01:17 +0200

    Add files via upload

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dnmes = double.Parse(Console.ReadLine());
+            double prden = double.Parse(Console.ReadLine());
+            double dolar = double.Parse(Console.ReadLine());
+            double msz = dnmes * prden;
+            double gdo = (msz * 12) + (msz * 2.5);
+            double dn = gdo %= 25;
+            double cdn = gdo - dolar;
+            double dnsr  = cdn / 365 ;
+            Console.WriteLine(dnsr);
+        }
+    }
+}
